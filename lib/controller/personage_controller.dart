9f13aa4eaa30102d8c463df5497dc2a2badd9652
Arthurlima544 <arthurlindaolima544@gmail.com>
2023-04-30 00:20:13 -0300

    adding personage  controller

---
+++ lib/controller/personage_controller.dart
@@ -0,0 +1,40 @@
+import 'package:app/data_structures/bfs.dart';
+import 'package:app/data_structures/vertices.dart';
+import 'package:app/main.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+const initialCharposition = Tuple2(1, 1);
+const winGamePosition = Tuple2(29, 29);
+
+class PersonagemController extends ValueNotifier<Tuple2<int, int>> {
+  PersonagemController(super.value);
+
+  late List<Vertice<Color>>? solvedGrafo;
+
+  void update(int x, int y) {
+    value = Tuple2(x, y);
+  }
+
+  Future<void> runBFS() async {
+    print("Acheiiiiiiiiiiii o grafooo");
+    solvedGrafo = await matriz.value1.breadthFirstSearch(
+        Vertice(
+            indexX: initialCharposition.head,
+            indexY: initialCharposition.tail,
+            dado: Colors.red),
+        Vertice(
+            indexX: winGamePosition.head,
+            indexY: winGamePosition.tail,
+            dado: Colors.yellow));
+  }
+
+  Future<void> comecaAandar() async {
+    for (var vertice in solvedGrafo!) {
+      await Future.delayed(const Duration(seconds: 1));
+      print(
+          "Andou no ${vertice.indexX} x ${vertice.indexY} : Tamanho: $solvedGrafo");
+      personagemPositionController.update(vertice.indexX, vertice.indexY);
+    }
+  }
+}
