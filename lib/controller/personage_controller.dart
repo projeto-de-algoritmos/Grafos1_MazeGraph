4c67a8b6eb7cb86eb0759dd141cdd95470ab2645
Arthurlima544 <arthurlindaolima544@gmail.com>
2023-05-01 12:53:37 -0300

    nova forma de visualizar a Ui
    Co-authored-by: Davi Lima <DaviSilva25@users.noreply.github.com>

--- lib/controller/personage_controller.dart
+++ lib/controller/personage_controller.dart
@@ -28,15 +28,20 @@ class PersonagemController extends ValueNotifier<Tuple2<int, int>> {
     final lastVertice = Vertice(
       indexX: winGamePosition.head,
       indexY: winGamePosition.tail,
-      dado: Colors.yellow,
+      dado: Colors.transparent,
     );
-    solvedGrafo = await matriz.value1.breadthFirstSearchWithFinal(
+    solvedGrafo = await matriz.value1.bfs(
         Vertice(
           indexX: initialCharposition.head,
           indexY: initialCharposition.tail,
-          dado: Colors.red,
+          dado: Colors.transparent,
         ),
         lastVertice);
+
+    print(solvedGrafo);
+/*     for (var i in solvedGrafo!) {
+      print(i);
+    } */
 
     if (solvedGrafo!.contains(lastVertice)) {
       return true;
@@ -49,13 +54,13 @@ class PersonagemController extends ValueNotifier<Tuple2<int, int>> {
     Tuple2 oldPosition = const Tuple2(1, 1);
     for (Vertice<Color> vertice in solvedGrafo!) {
       //Se ele ta na posição inicial não alteramos nada
-      colorMatriz[oldPosition.value1][oldPosition.value2] = Colors.transparent;
+      colorMatriz[oldPosition.value1][oldPosition.value2] = Colors.green;
       colorMatriz[vertice.indexX][vertice.indexY] = Colors.red;
       update(vertice.indexX, vertice.indexY);
       /* print(
           "Posição anterior: $oldPosition ${colorMatriz[oldPosition.value1][oldPosition.value2]} | Posicao Atual: ${vertice.indexX} x ${vertice.indexY} ${colorMatriz[vertice.indexX][vertice.indexY]}"); */
       oldPosition = Tuple2(vertice.indexX, vertice.indexY);
-      await Future.delayed(const Duration(milliseconds: 100));
+      await Future.delayed(const Duration(milliseconds: 50));
       if (vertice == lastVertice) break;
     }
   }
